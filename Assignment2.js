e13e1e7ac0dc6eccd7eae710b20f45a8a9201a42
Onkar Raghunath Shelke <onkarraghunathshelke@gamil.com>
2023-09-10 11:43:27 +0530

    added implementation for Assignment2.js

--- Assignment2.js
+++ Assignment2.js
@@ -1,1 +1,20 @@
-console.log("Hello Git i am Coming");
+let dayNumber = 9;
+let dayType;
+
+switch (dayNumber) {
+  case 1:
+  case 7:
+    dayType = "Weekend";
+    break;
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    dayType = "Weekday";
+    break;
+  default:
+    dayType = "Invalid day number";
+}
+
+console.log(`Day ${dayNumber} is a ${dayType}.`);
